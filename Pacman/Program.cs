05228bafe679ee41420234459883179a0e00fcb2
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-02 22:15:57 +0300

    Pacman. Project started

---
+++ Pacman/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        char[,] map = null;
+        string[] file = File.ReadAllLines("map.txt");
+        Console.WriteLine(file[0]);
+    }
+
+    private static char[,] ReadMap(string path)
+    {
+        string[] file = File.ReadAllLines(path);
+        
+    }
+}
