0c198b9b54e2443c5059739f0cc946abaef5e6fe
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-04 00:57:37 +0300

    GunShop. Project started (also added gitignore)

---
+++ GunShop/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        List<Guns> guns = new List<Guns>();
+        Console.WriteLine(guns.Count);
+        
+    }
+
+    class Guns
+    {
+        "AK47",2600
+        "M16", 4200
+    } 
+}
