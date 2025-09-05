f03f117e8fcf7b52bddcfde0327c30a3457e40eb
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-05 21:08:49 +0300

    GunShop. Learning OOP - created constructor Gun

--- GunShop/Program.cs
+++ GunShop/Program.cs
@@ -1,15 +1,28 @@
-﻿class Program
+﻿using System.Globalization;
+
+class Program
 {
     public static void Main()
     {
-        List<Guns> guns = new List<Guns>();
-        Console.WriteLine(guns.Count);
-        
+        // Console.WriteLine(name + " - " + "$" + price);
+    }
+}
+class Guns
+{
+    Gun ak47 = new Gun("AK47", 2600);
+    Gun ak48 = new Gun("AK47", 2600);
+    Gun ak49 = new Gun("AK47", 2600);
+    Gun ak410 = new Gun("AK47", 2600);
+}
+class Gun
+{
+    public string name;
+    public int price;
+
+    public Gun(string name, int price)
+    {
+        this.name = name;
+        this.price = price;
     }
 
-    class Guns
-    {
-        "AK47",2600
-        "M16", 4200
-    } 
-}
+} 
